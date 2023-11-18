664c68688d2eee025ccd73f6dfe509a7e2bdff13
Gofmet <125132805+Gofmet@users.noreply.github.com>
2023-11-18 16:36:13 +0300

    Add files via upload

--- copescript.js
+++ copescript.js
@@ -1,9 +1,9 @@
 function copyToClipboard() {
     // Получаем элемент с id "link"
-    var linkElement = document.getElementById("link");
+    let linkElement = document.getElementById("link");
 
     // Создаем элемент для временного хранения текста
-    var tempInput = document.createElement("input");
+    let tempInput = document.createElement("input");
 
     // Устанавливаем значение элемента в текст из элемента "link"
     tempInput.value = linkElement.innerText;
@@ -21,3 +21,40 @@ function copyToClipboard() {
     document.body.removeChild(tempInput);
 
 }
+$("#dateTo").datetimepicker(
+    {
+        format: 'DD.MM.YYYY HH:mm',
+        inline: false,
+        locale: 'ru',
+    }
+);
+
+document.getElementById("copybutton").addEventListener("click", function() {
+    copyToClipboardd();
+});
+
+function copyToClipboardd() {
+    // Получаем элемент с id "link"
+    let linkElement = document.getElementById("link");
+
+    // Создаем элемент для временного хранения текста
+    let tempInput = document.createElement("input");
+
+    // Устанавливаем значение элемента в текст из элемента "link"
+    tempInput.value = linkElement.innerText;
+
+    // Добавляем элемент в DOM
+    document.body.appendChild(tempInput);
+
+    // Выделяем текст в элементе
+    tempInput.select();
+
+    // Копируем выделенный текст в буфер обмена
+    document.execCommand("copy");
+
+    // Удаляем временный элемент
+    document.body.removeChild(tempInput);
+
+}
+
+
